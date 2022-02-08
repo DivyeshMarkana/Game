0c091a4ddce5b9120eeb383327d3f1ee16819e6d
Divyesh Markana <divyeshmarkana724@gmail.com>
2022-02-08 19:47:14 +0530

    add result screen

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+const game = document.getElementById("game")
 const cards = document.querySelectorAll(".memory-card")
+const frontFace = document.querySelectorAll(".front-face")
+const backFace = document.querySelectorAll(".back-face")
+
+
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard;
 let secondCard;
+let flipCount = 6;
+let userFlips = 0;
 
 function flipCard() {
     if (lockBoard) return;
-    if (firstCard === this)return;
+    if (firstCard === this) return;
 
     this.classList.add("flip");
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-        
+
         return;
     }
 
@@ -24,8 +31,51 @@ function flipCard() {
 }
 
 function checkForMatch() {
- let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
- isMatch ? disableCards() : unflipCards();
+    //  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    //  isMatch ? disableCards() : unflipCards();
+
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards()
+        userFlips++
+        if (userFlips === flipCount) {
+            //    document.body.style.backgroundColor = "white"
+            game.classList.toggle("game__result")
+            game.innerHTML = `  
+            <div class="result">
+                <h4>🥳🥳🥳</h4>
+                <h4>You win the Game!</h4>
+                <button class="start__btn">Start Game</button>
+             </div>`
+            //  button for result
+             
+            const startBtn = document.querySelector(".start__btn")
+            console.log(startBtn);
+
+
+            cards.forEach(card => {
+                card.classList.toggle("card__shadow")
+            });
+
+            frontFace.forEach(face => {
+                face.classList.toggle("card__faces")
+            });
+
+            backFace.forEach(face => {
+                face.classList.toggle("card__faces")
+            });
+
+            startBtn.addEventListener("click", () => {
+                document.location.reload();
+            })
+
+        
+
+
+        }
+    } else {
+        unflipCards()
+    }
+
 }
 
 function disableCards() {
@@ -39,7 +89,7 @@ function unflipCards() {
 
     lockBoard = true;
 
-    setTimeout( () => {
+    setTimeout(() => {
         firstCard.classList.remove("flip")
         secondCard.classList.remove("flip")
 
